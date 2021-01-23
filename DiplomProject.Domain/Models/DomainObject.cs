ed222cb29d2de36d9a7041a77c9630e06ac7917e
Алексей Тимофеев <garhafar@gmail.com>
2021-01-23 16:44:06 +0300

    Created layers

---
+++ DiplomProject.Domain/Models/DomainObject.cs
@@ -0,0 +1,7 @@
+﻿namespace DiplomProject.Domain.Models
+{
+    public class DomainObject
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ DiplomProject.Domain/Services/IDataService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DiplomProject.Domain.Services
+{
+    public interface IDataService<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> Get(int id);
+        Task<T> Create(T entity);
+        Task<bool> Delete(int id);
+        Task<T> Update(int id, T entity);
+    }
+}
